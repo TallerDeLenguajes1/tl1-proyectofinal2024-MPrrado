9257f2533d5f465716047d0f526343b0aa5264a0
Prado Matias Santiago <matias14prado@gmail.com>
2024-06-14 20:06:37 -0300

    FabricacionPersonajes v1.0.0

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System.Collections;
+using System.Data;
+using Personajes_Y_Estadisticas;
+OperacionPersonajes operar = new OperacionPersonajes();
+const int CANTIDAD_PERSONAJES_CREAR = 10;
+Personaje[] personajes = new Personaje[CANTIDAD_PERSONAJES_CREAR];
+System.Console.WriteLine();
+System.Console.WriteLine();
+Console.WriteLine("---------------Bienvenido al juego Worms World Sharp---------------");
+for (int i = 0; i < CANTIDAD_PERSONAJES_CREAR; i++)
+{
+    personajes[i] = operar.FabricaDePersonajes(i);
+}
+
+Console.ForegroundColor = ConsoleColor.Red;
+System.Console.WriteLine();
+System.Console.WriteLine();
+System.Console.WriteLine("----------PERSONAJES----------");
+System.Console.WriteLine();
+System.Console.WriteLine();
+int k=1;
+foreach (Personaje personaje in personajes)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    System.Console.WriteLine($"----PERSONAJE [{k}]----");
+    k++;
+    Console.ForegroundColor = ConsoleColor.Green;
+    System.Console.WriteLine($"\t---->{personaje.Datos.Nombre}----");
+    System.Console.WriteLine($"\t---->{personaje.Datos.Tipo}----");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    System.Console.WriteLine($"\t----Caracteristicas----");
+    System.Console.WriteLine($"\t---->Salud: {personaje.Caracteristicas.Salud}----");
+    System.Console.WriteLine($"\t---->Armadura: {personaje.Caracteristicas.Armadura}----");
+    System.Console.WriteLine($"\t---->Ataque: {personaje.Caracteristicas.Ataque}----");
+    System.Console.WriteLine();
+    System.Console.WriteLine();
+}
+Console.ForegroundColor = ConsoleColor.White;
--- personajes.cs
+++ personajes.cs
@@ -2,15 +2,17 @@ namespace Personajes_Y_Estadisticas
 {
     public enum Tipo
     {
-        KAMIKAZE,
-        MEDICO,
-        BOMBARDERO,
-        GUERRERO
+        KAMIKAZE = 0,
+        MEDICO = 1,
+        BOMBARDERO = 2,
+        GUERRERO = 3
     }
     public class Personaje
     {
+        // private Datos? datos;
+        // private Caracteristicas? caracteristicas;
+        private Caracteristicas? caracteristicas; 
         private Datos? datos;
-        private Caracteristicas? caracteristicas;
 
         public Datos Datos { get => datos; set => datos = value; }
         public Caracteristicas? Caracteristicas { get => caracteristicas; set => caracteristicas = value; }
@@ -25,6 +27,9 @@ namespace Personajes_Y_Estadisticas
             this.tipo = tipo;
             this.nombre = nombre;
         }
+
+        public Tipo Tipo { get => tipo; }
+        public string? Nombre { get => nombre;}
         // private frase celebre con API?
     }
 
@@ -33,5 +38,29 @@ namespace Personajes_Y_Estadisticas
         private int ataque;
         private int armadura;
         private int salud;
+
+        public int Ataque { get => ataque; set => ataque = value; }
+        public int Armadura { get => armadura; set => armadura = value; }
+        public int Salud { get => salud; set => salud = value; }
+    }
+
+    public class OperacionPersonajes
+    {
+        public Personaje FabricaDePersonajes(int numeroPersonaje)
+        {
+            Personaje personaje = new Personaje();
+            personaje.Caracteristicas = new Caracteristicas();
+            Random randomTipo = new Random();
+            Random randomAtaque_Armadura= new Random();
+            // Random randomSalud = new Random();
+            // Random randomNombre = new Random();
+            // personaje.Datos = new Datos(Tipo.KAMIKAZE, $"nombre_API_{i}");
+            personaje.Datos = new Datos((Tipo)randomTipo.Next(0,4), $"nombre_API_{numeroPersonaje+1}");
+            personaje.Caracteristicas.Armadura = randomAtaque_Armadura.Next(1,10);
+            personaje.Caracteristicas.Ataque = randomAtaque_Armadura.Next(1,10);
+            personaje.Caracteristicas.Salud = 100;
+
+            return personaje;
+        }
     }
 }
